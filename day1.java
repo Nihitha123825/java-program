46bb18bb28b9a09c04a57c1947f3f5a9bce5727d
Nihitha123825 <nihitha025@gmail.com>
2026-04-29 12:07:30 +0530

    day1

---
+++ day1.java
@@ -0,0 +1,15 @@
+class day1
+{
+    public static void main(String args[])
+    {
+        int a = 10;
+        String s1 = "abc";
+        String s2 = "abcd";
+        System.out.println(s1 == s2);
+        System.out.print(a);
+
+        System.out.print("hello");
+        System.out.println("hi");
+        System.out.println("nii");
+    }
+}
