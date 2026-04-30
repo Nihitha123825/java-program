041570e4e94df00f4f0674a407eafd876edab39e
Nihitha123825 <nihitha025@gmail.com>
2026-04-30 14:44:51 +0530

    scanner input

---
+++ input.java
@@ -0,0 +1,55 @@
+import java.util.*;
+class input
+{
+    public static void main(String args[])
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter name: ");
+        String name = s.nextLine();
+        System.out.println(name);
+        System.out.println("Enter age: ");
+        int age =  s.nextInt();
+        System.out.println(age);
+        s.nextLine();
+        System.out.println("Enter email: ");
+         String email = s.nextLine();
+        System.out.println(email);
+        System.out.println("Enter mark1: ");
+        int mark1 = s.nextInt();
+        System.out.println(mark1);
+
+        System.out.println("Enter mark2: ");
+        int mark2 = s.nextInt();
+        System.out.println(mark2);
+        System.out.println("Enter avg: ");
+        float avg = s.nextFloat();
+        System.out.println(avg);
+        System.out.println("Enter long number: ");
+        long num = s.nextLong();
+        System.out.println(num);
+        System.out.println("enter double number");
+        double num2 = s.nextDouble();
+        System.out.println(num2);
+
+        
+        
+        
+        
+    
+       
+        
+       
+        
+        
+        
+        
+        
+
+        
+
+
+
+
+
+    }
+}
