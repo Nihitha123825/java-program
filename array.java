fb123dcd3131a3f502812221dfb12a725391158a
Nihitha123825 <nihitha025@gmail.com>
2026-05-02 11:17:17 +0530

    array

---
+++ array.java
@@ -0,0 +1,14 @@
+class array
+{
+     public static void main(String args[])
+    {
+        int a[] = {1,2,3,4,5};
+        int arr[] = a.clone();
+        for(int i =0 ; i<arr.length;i++)
+        {
+System.out.println(arr[i]);
+
+  
+        }
+    }
+}
