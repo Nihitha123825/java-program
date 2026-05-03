a75e0badb9d8aa21ca1c2f227e4a07ddc193d166
Nihitha123825 <nihitha025@gmail.com>
2026-05-03 10:23:24 +0530

    random functions

---
+++ rm.java
@@ -0,0 +1,33 @@
+class rm{
+    public static void main(String args[])
+    {
+        // int a = 200;
+        // int b = 100; 
+
+//System.out.println(Math.abs(a));
+ //max      
+//    System.out.println(Math.max(a,b));
+//    //min   
+//        System.out.println(Math.min(a,b));
+// //    power   
+//       System.out.println(Math.pow(a,b));
+//System.out.println(Math.sqrt(a)); // square
+//cube
+//  System.out.println(Math.cbrt(a));
+//ceil,floor,round
+
+// double n1 = 5.6;
+// double n2 = 6.6;
+// System.out.println(Math.ceil(n1));
+// System.out.println(Math.floor(n2));
+// System.out.println(Math.round(n2));
+//sin, tan,cos
+double a = 45;
+double rad = Math.toRadians(a);
+System.out.println("sin:" +Math.sin(rad));
+System.out.println("cos:" +Math.cos(rad));
+System.out.println("tan:" +Math.tan(rad));
+
+
+    }
+}
