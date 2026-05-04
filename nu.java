b5ac3f58ef3f45e64ce4f1371f9d73afe08ed8c9
Nihitha123825 <nihitha025@gmail.com>
2026-05-04 11:15:11 +0530

    null

---
+++ nu.java
@@ -0,0 +1,12 @@
+class nu {
+    public static void main(String[] args) {
+        String str = null;
+
+        try {
+            System.out.println(str.length());
+        } 
+        catch (NullPointerException e) {
+            System.out.println("Null exception occurred");
+        }
+    }
+}
